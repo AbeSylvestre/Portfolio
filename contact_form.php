29a9a9e79ddc61939fb5252d53d1f5da3dd3faa2
AbeSylvestre <abraham.sylvestre@gmail.com>
2016-03-16 22:01:11 -0400

    Initial Live edition
    Added missing viewport metatag, and button hovers; other styling.

--- contact_form.php
+++ contact_form.php
@@ -4,9 +4,9 @@ $contactName = $_POST['contactName'];
 $contactEmail = $_POST['contactEmail'];
 $message = $_POST['message'];
 $to = "abraham.sylvestre@gmail.com";
-$subject = "New Portfolio Message!";
+$subject = "Portfolio message from: " . $contactName;
 
-mail($to, $subject, $message, "From: " . $contactName);
+mail($to, $subject, $message, "From: " . $contactEmail);
 echo "Your message has been sent; I hope to speak with you soon! -Abe";
 
 ?>
